b3e3363f33ee3c5b3b42a69b2ec7a1e7ca16c1b7
Alan Webb <webbalan91939@gmail.com>
2026-03-02 23:43:18 +0000

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,19 +67,21 @@ const Navbar = ({activeSection}) => {
 								stroke="currentColor"
 								aria-hidden="true">
 								{/* Conditional Rendering */}
-								{/* <path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M6 18L18 6M6 6l12 12"
-								/>
-
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M4 6h16M4 12h16M4 18h16"
-								/> */}
+								{isOpen ? (
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M6 18L18 6M6 6l12 12"
+									/>
+								) : (
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M4 6h16M4 12h16M4 18h16"
+									/>
+								)}
 							</svg>
 						</button>
 					</div>
