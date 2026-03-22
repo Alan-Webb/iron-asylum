f7fee3f0b7ee290822defe9e56ceb15075d5c5d7
Alan Webb <webbalan91939@gmail.com>
2026-03-22 08:26:23 +0000

    Update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import Training from "./components/Training";
+import Pricing from "./components/Pricing";
+import Community from "./components/Community";
+import Faq from "./components/Faq";
+import Join from "./components/Join";
+import Footer from "./components/Footer";
+
 const App = () => {
-	return <div>Iron Asylum</div>;
+	return (
+		<div className="max-w-360 mx-auto bg-page overflow-hidden relative">
+			<Header />
+			<Banner />
+			<About />
+			<Training />
+			<Pricing />
+			<Community />
+			<Faq />
+			<Join />
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/constants/data.js
@@ -0,0 +1,25 @@
+import Logo from "../src/assets/logo.webp";
+
+export const header = {
+	logo: Logo,
+	btnLoginText: "Log in",
+	btnSignupText: "Sign Up",
+};
+
+export const nav = [
+	{name: "Home", href: "/"},
+	{name: "About", href: "/"},
+	{name: "Training", href: "/"},
+	{name: "Pricing", href: "/"},
+	{name: "Community", href: "/"},
+	{name: "FAQ", href: "/"},
+];
+
+export const banner = {
+	titlePart1: "Get the best out of your droid",
+	titlePart2: "– the best out of you.",
+	subtitle:
+		"We provide cutting edge droid training but with a focus on bonding.",
+	textBtn: "Join Now",
+	img: "",
+};
