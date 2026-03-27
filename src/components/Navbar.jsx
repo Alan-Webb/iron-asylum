b00e406df697f32cb0a3ce2c4b6b54e2bf3ea07c
Alan Webb <webbalan91939@gmail.com>
2026-03-27 01:23:41 +0000

    Update Navbar & Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,34 @@
+import {Link} from "react-router";
+import {GiCyborgFace} from "react-icons/gi";
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="w-full h-auto bg-transparent backdrop-blur-lg shadow-none lg:px-24 md:px-16 sm:px-6 px-4 z-50">
+			<div className="justify-between mx-auto lg:w-full md:items-center md:flex">
+				{/* Navbar & Toggle */}
+				<div>
+					<div className="flex items-center justify-between py-1 md:py-1 md:block">
+						{/* Logo */}
+						<Link
+							to="/"
+							className="text-4xl text-red-600 font-semibold tracking-[o.1rem] flex items-end gap-x-1 relative">
+							IR
+							<span className=" text-neutral-100">
+								<GiCyborgFace className="h-10 w-8" />
+							</span>
+							N<span className="text-neutral-400">ASYLUM</span>
+						</Link>
+
+						{/* Toggle */}
+						<div className="md:hidden">
+							<button className="p-2 text-neutral-700 outline-none focus:outline-none border-none cursor-pointer"></button>
+						</div>
+					</div>
+				</div>
+				{/* Navbar Items & Btn */}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
 	return (
 		<div className="w-full min-h-screen bg-neutral-950 flex flex-col">
 			{/* Hero + Navbar Section*/}
 			<div className="w-full md:h-screen h-auto flex flex-col relative space-y-0 bg-cover bg-center bg-[url('./assets/HeroImg.webp')] bg-no-repeat">
+				{/* Overlay */}
+				<div className="w-full h-full absolute top-0 left-0 bg-linear-to-r from-neutral-950/80 to-neutral-950/50"></div>
 				{/* Navbar */}
+				<Navbar />
 				{/* Hero */}
 			</div>
 			{/* Other Sections */}
-      <div className="md:space-y-24 space-y-14"></div>
+			<div className="md:space-y-24 space-y-14"></div>
 		</div>
 	);
 };
