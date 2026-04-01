acae7c502b1f833f41dc4842bcfd5795fd906b71
Alan Webb <webbalan91939@gmail.com>
2026-04-01 09:23:39 +0100

    Update Navbar & Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,54 @@
+import {FaDumbbell, FaPlay} from "react-icons/fa";
+import {GiWeight} from "react-icons/gi";
+import {FaHeartCirclePlus} from "react-icons/fa6";
+
 const Hero = () => {
 	return (
 		<div>
 			{/* Banner Section */}
 			<div
 				id="home"
-				className="relative h-screen bg-black overflow-hidden flex flex-col lg:flex-row items-center justify-center lg:justify-between"></div>
+				className="relative h-screen bg-black overflow-hidden flex flex-col lg:flex-row items-center justify-center lg:justify-between">
+				{/* Content */}
+				<div className="relative z-10 flex flex-col justify-center items-start px-6 sm:px-10 lg:px-20 text-white bg-linear-to-r from-black via-transparent h-full w-full lg:w-1/2">
+					<h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold uppercase mb-6 animate-slideInLeft text-center lg:text-left">
+						Turn grit into <span className="text-yellow-500">greatness</span>
+					</h1>
+					<p className="text-sm sm:text-base md:text-lg lg:text-xl mb-8 animate-slideInLeft delay-200 text-center lg:text-left">
+						Focused on strength and power, our platform delivers focused
+						training programs, expert guidance, and proven methods to help you
+						succeed.
+					</p>
+					{/* Icons */}
+					<div className="flex flex-wrap sm:flex-row gap-4 justify-center lg:justify-start mb-8 animate-fadeIn delay-400">
+						{/* Strength */}
+						<div className="flex items-center gap-3 group">
+							<FaDumbbell className="text-yellow-500 text-2xl sm:text-3xl transition-transform transform group-hover:rotate-12 group-hover:scale-110" />
+							<span className="text-sm sm:text-lg font-medium">Strength</span>
+						</div>
+						{/* Power */}
+						<div className="flex items-center gap-3 group">
+							<GiWeight className="text-yellow-500 text-2xl sm:text-3xl transition-transform transform group-hover:rotate-12 group-hover:scale-110" />
+							<span className="text-sm sm:text-lg font-medium">Power</span>
+						</div>
+						{/* Endurance */}
+						<div className="flex items-center gap-3 group">
+							<FaHeartCirclePlus className="text-yellow-500 text-2xl sm:text-3xl transition-transform transform group-hover:rotate-12 group-hover:scale-110" />
+							<span className="text-sm sm:text-lg font-medium">Endurance</span>
+						</div>
+					</div>
+					{/* Buttons*/}
+					<div className="flex flex-wrap gap-4 justify-center lg:justify-start mb-6">
+						<button className="px-4 sm:px-6 py-2 sm:py-3 bg-yellow-400 text-black font-semibold rounded-full shadow-md hover:bg-yellow-500 transition-transform transform hover:scale-105 cursor-pointer delay-100">
+							Get Started
+						</button>
+						<button className="px-4 sm:px-6 py-2 sm:py-3 bg-white text-black font-semibold rounded-full shadow-md hover:bg-stone-300 transition-transform transform hover:scale-105 cursor-pointer delay-100 flex items-center gap-2">
+							<FaPlay className="text-xl" />
+							Watch Demo
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,13 @@ const Navbar = () => {
 		<nav className="fixed top-0 left-0 w-full bg-linear-to-r from-black via-gray-900 to-black border-b border-stone-300 backdrop-blur-md shadow-lg z-50">
 			<div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-3">
 				{/* Logo */}
-				<div className="flex space-x-4 cursor-pointer text-2xl font-bold text-white uppercase">
+				<div className="flex space-x-4 cursor-pointer text-2xl font-bold text-yellow-500 uppercase">
 					<img
 						src="/src/assets/logo.svg"
 						alt="iron asylum logo"
 						className="bg-white rounded-full border border-white w-8"
 					/>
-					<span className="text-red-500">Iron</span>
+					<span className="text-white">Iron</span>
 					Asylum
 				</div>
 				{/* Desktop Links */}
@@ -32,7 +32,7 @@ const Navbar = () => {
 						to="home"
 						smooth={true}
 						duration={500}
-						className="flex items-center gap-2 hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="flex items-center gap-2 hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						<FaHome />
 						Home
 					</Link>
@@ -41,7 +41,7 @@ const Navbar = () => {
 						to="trainers"
 						smooth={true}
 						duration={500}
-						className="flex items-center gap-2 hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="flex items-center gap-2 hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						<FaRegUserCircle />
 						Trainers
 					</Link>
@@ -50,7 +50,7 @@ const Navbar = () => {
 						to="opinions"
 						smooth={true}
 						duration={500}
-						className="flex items-center gap-2 hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="flex items-center gap-2 hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						<FaCommentDots />
 						Opinions
 					</Link>
@@ -59,7 +59,7 @@ const Navbar = () => {
 						to="contact"
 						smooth={true}
 						duration={500}
-						className="flex items-center gap-2 hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="flex items-center gap-2 hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						<FaEnvelope />
 						Contact
 					</Link>
@@ -68,7 +68,7 @@ const Navbar = () => {
 				<button
 					onClick={() => setMenuOpen(!menuOpen)}
 					aria-label="Toggle Menu"
-					className="md:hidden text-2xl text-white hover:text-red-500 focus:outline-none cursor-pointer">
+					className="md:hidden text-2xl text-white hover:text-yellow-500 focus:outline-none cursor-pointer">
 					{menuOpen ? <IoMdClose /> : <FaBars />}
 				</button>
 			</div>
@@ -81,7 +81,7 @@ const Navbar = () => {
 						smooth={true}
 						duration={500}
 						onClick={() => setMenuOpen(false)}
-						className="hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						Home
 					</Link>
 					{/* Trainers */}
@@ -90,7 +90,7 @@ const Navbar = () => {
 						smooth={true}
 						duration={500}
 						onClick={() => setMenuOpen(false)}
-						className="hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						Trainers
 					</Link>
 					{/* Opinions */}
@@ -99,7 +99,7 @@ const Navbar = () => {
 						smooth={true}
 						duration={500}
 						onClick={() => setMenuOpen(false)}
-						className="hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						Opinions
 					</Link>
 					{/* Contact */}
@@ -108,7 +108,7 @@ const Navbar = () => {
 						smooth={true}
 						duration={500}
 						onClick={() => setMenuOpen(false)}
-						className="hover:text-red-500 transition-colors duration-300 cursor-pointer">
+						className="hover:text-yellow-500 transition-colors duration-300 cursor-pointer">
 						Contact
 					</Link>
 				</div>
