8ae75b0c4ef2773c945196dc52e7fc883e31ad91
Alan Webb <webbalan91939@gmail.com>
2026-04-04 14:21:26 +0100

    Create & update Wcu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Wcu from "./components/Wcu";
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Wcu />
 		</>
 	);
 };
---
+++ src/components/Wcu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Wcu = () => {
+	return (
+		<div className="bg-linear-to-b from-black via-gray-800 to-black py-12">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				{/* Heading */}
+				<div className="text-center mb-12">
+					<h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold bg-linear-to-r from-pink-300 via-indigo-300 to-teal-300 text-transparent bg-clip-text">
+						Why Choose Us
+					</h2>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Wcu;
